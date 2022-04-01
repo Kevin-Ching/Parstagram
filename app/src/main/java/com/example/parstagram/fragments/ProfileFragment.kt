e5cc4264edf3e9572bf1df4c13b783af1d77af8f
Kevin Ching <keching@berkeley.edu>
2022-04-01 00:45:58 -0700

    Added username to profile feed, changed scaleType of image in posts, moved buttons up in login screen

--- app/src/main/java/com/example/parstagram/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/parstagram/fragments/ProfileFragment.kt
@@ -1,13 +1,35 @@
 package com.example.parstagram.fragments
 
+import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import com.example.parstagram.Post
+import com.example.parstagram.R
 import com.parse.FindCallback
 import com.parse.ParseException
 import com.parse.ParseQuery
 import com.parse.ParseUser
 
 class ProfileFragment : FeedFragment() {
+
+    lateinit var tvUsername: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.profile_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        tvUsername = view.findViewById(R.id.tvUsername)
+        tvUsername.text = ParseUser.getCurrentUser().username
+        super.onViewCreated(view, savedInstanceState)
+    }
 
     override fun queryPosts() {
         // Specify which class to query
